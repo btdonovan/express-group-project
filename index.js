a8cc40ccd14f815cc50f7d92bf67b392074e32a9
Benjamin Todd Donovan <carcel@navonod.us>
2020-06-23 15:06:49 -0600

    stretch goal 3 complete

--- index.js
+++ index.js
@@ -2,33 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fs = require('fs')
-const songs = JSON.parse(fs.readFileSync('songs.json'))
+const tracks = JSON.parse(fs.readFileSync('songs.json'))
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 app.listen(port, () => console.log(`server started at http://localhost:${port}`))
 
 
-app.get('/', (req, res) => {
-  res.send(songs)
+app.get('/songs', (req, res) => {
+  res.send(tracks.filter(song => ((song.kind === 'song'))))
 })
 
 app.get('/songbyid/:trackId', (req, res) => {
-    res.send(songs.filter(song => ((song.trackId === Number(req.params.trackId)) && (song.kind === 'song'))))
+    res.send(tracks.filter(song => ((song.trackId === Number(req.params.trackId)) && (song.kind === 'song'))))
 })
 
 app.get('/songbyname/:trackName', (req, res) => {
-  res.send(songs.filter(song => ((song.trackName.toLowerCase() === req.params.trackName.toLowerCase()) && (song.kind === 'song'))))
+  res.send(tracks.filter(song => ((song.trackName.toLowerCase() === req.params.trackName.toLowerCase()) && (song.kind === 'song'))))
 })
 
 app.get('/songsbyalbumid/:collectionId', (req, res) => {
-  res.send(songs.filter(song => ((song.collectionId === Number(req.params.collectionId)) && (song.kind === 'song'))))
+  res.send(tracks.filter(song => ((song.collectionId === Number(req.params.collectionId)) && (song.kind === 'song'))))
 })
 
 app.get('/songsbyalbumname/:collectionName', (req, res) => {
-  // console.log(decodeURIComponent(req.params))
-  // console.log(req.params.collectionName.toLowerCase())
-  let result = songs.filter(song => {
+  let result = tracks.filter(song => {
     let collectionName
     if (!!song.collectionName) {
       collectionName = song.collectionName.toLowerCase()
@@ -42,7 +40,7 @@ app.get('/songsbyalbumname/:collectionName', (req, res) => {
 
 app.patch('/songbyid/:trackId', (req, res) => {
   let trackIndex
-  let track = songs.filter((song, index) => {
+  let track = tracks.filter((song, index) => {
     if (song.trackId === Number(req.params.trackId)) {
       trackIndex = index
       return true
@@ -51,13 +49,24 @@ app.patch('/songbyid/:trackId', (req, res) => {
     }
   })
   let newInfo = {...track[0], ...req.body}
-  songs[trackIndex] = newInfo
+  tracks[trackIndex] = newInfo
+})
+
+app.put ('/newsong', (req, res) => {
+  let newSong = req.body
+  let oldSongs = tracks.filter(song => song.trackId === req.body.trackId)
+  if (oldSongs.length === 0) {
+    tracks.push(newSong)
+    res.send('song added')
+  } else {
+    res.send('no song added')
+  }
 })
 
 app.delete('/songbyid/:trackId', (req, res) => {
   let trackIndex
   let found = false
-  let track = songs.filter((song, index) => {
+  let track = tracks.filter((song, index) => {
     if (song.trackId === Number(req.params.trackId)) {
       trackIndex = index
       found = true
@@ -67,7 +76,7 @@ app.delete('/songbyid/:trackId', (req, res) => {
     }
   })
   if (found) {
-    songs.splice(trackIndex, 1)
+    tracks.splice(trackIndex, 1)
     res.send('confirmed')
   } else {
     res.send('failed')
@@ -75,20 +84,93 @@ app.delete('/songbyid/:trackId', (req, res) => {
 })
 
 app.get('/songsbyartistid/:artistId', (req, res) => {
-  res.send(songs.filter(song => ((song.artistId === Number(req.params.artistId)) && (song.kind === 'song'))))
-})
-
-app.put ('/newsong', (req, res) => {
-  let newSong = req.body
-  let oldSongs = songs.filter(song => song.trackId === req.body.trackId)
-  if (oldSongs.length === 0) {
-    songs.push(newSong)
-    res.send('song added')
-  } else {
-    res.send('no song added')
-  }
+  res.send(tracks.filter(song => ((song.artistId === Number(req.params.artistId)) && (song.kind === 'song'))))
 })
 
 app.get('/songsbyartistname/:artistName', (req, res) => {
-    res.send(songs.filter(song => ((song.artistName.toLowerCase().includes(req.params.artistName.toLowerCase())) && (song.kind === 'song'))))
+    res.send(tracks.filter(song => ((song.artistName.toLowerCase().includes(req.params.artistName.toLowerCase())) && (song.kind === 'song'))))
+  })
+
+
+  //video 'music-video'
+app.get('/videos', (req, res) => {
+  res.send(tracks.filter(video => ((video.kind === 'music-video'))))
+})
+
+app.get('/videobyid/:trackId', (req, res) => {
+    res.send(tracks.filter(video => ((video.trackId === Number(req.params.trackId)) && (video.kind === 'music-video'))))
+})
+
+app.get('/videobyname/:trackName', (req, res) => {
+  res.send(tracks.filter(video => ((video.trackName.toLowerCase() === req.params.trackName.toLowerCase()) && (video.kind === 'music-video'))))
+})
+
+app.get('/videosbyalbumid/:collectionId', (req, res) => {
+  res.send(tracks.filter(video => ((video.collectionId === Number(req.params.collectionId)) && (video.kind === 'music-video'))))
+})
+
+app.get('/videosbyalbumname/:collectionName', (req, res) => {
+  let result = tracks.filter(video => {
+    let collectionName
+    if (!!video.collectionName) {
+      collectionName = video.collectionName.toLowerCase()
+      return ((collectionName === req.params.collectionName.toLowerCase()) && (video.kind === 'music-video'))
+    } else {
+      return false
+    }
+  })
+  res.send(result)
+})
+
+app.patch('/videobyid/:trackId', (req, res) => {
+  let trackIndex
+  let track = tracks.filter((video, index) => {
+    if (video.trackId === Number(req.params.trackId)) {
+      trackIndex = index
+      return true
+    } else {
+      return false
+    }
+  })
+  let newInfo = {...track[0], ...req.body}
+  tracks[trackIndex] = newInfo
+})
+
+app.put ('/newvideo', (req, res) => {
+  let newvideo = req.body
+  let oldvideos = tracks.filter(video => video.trackId === req.body.trackId)
+  if (oldvideos.length === 0) {
+    tracks.push(newvideo)
+    res.send('video added')
+  } else {
+    res.send('no video added')
+  }
+})
+
+app.delete('/videobyid/:trackId', (req, res) => {
+  let trackIndex
+  let found = false
+  let track = tracks.filter((video, index) => {
+    if (video.trackId === Number(req.params.trackId)) {
+      trackIndex = index
+      found = true
+      return true
+    } else {
+      return false
+    }
+  })
+  if (found) {
+    tracks.splice(trackIndex, 1)
+    res.send('confirmed')
+  } else {
+    res.send('failed')
+  }
+})
+
+app.get('/videosbyartistid/:artistId', (req, res) => {
+  res.send(tracks.filter(video => ((video.artistId === Number(req.params.artistId)) && (video.kind === 'music-video'))))
+})
+
+app.get('/videosbyartistname/:artistName', (req, res) => {
+    res.send(tracks.filter(video => ((video.artistName.toLowerCase().includes(req.params.artistName.toLowerCase())) && (video.kind === 'music-video'))))
   })
